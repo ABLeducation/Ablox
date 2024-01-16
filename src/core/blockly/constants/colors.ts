1a85fafab66abb8ccb00123c216590aaa51ed6fb
ABLeducation <155818284+ABLeducation@users.noreply.github.com>
2024-01-16 17:40:58 +0530

    Update colors.ts

--- src/core/blockly/constants/colors.ts
+++ src/core/blockly/constants/colors.ts
@@ -1,8 +1,8 @@
 export enum COLOR_THEME {
-  SENSOR = '#505bda',
-  ARDUINO_START_BLOCK = '#b063c5',
+  SENSOR = '#00FFFF',
+  ARDUINO_START_BLOCK = '#0000FF',
   COMPONENTS = '#512c62',
-  ARDUINO = '#b063c5',
+  ARDUINO = '#008000',
   DATA = '#ffaac3',
   VALUES = '#505bda',
   CONTROL = '#b063c5',
