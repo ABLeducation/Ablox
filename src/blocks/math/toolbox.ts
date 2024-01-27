6d185d937a01fe0237a57b18c9e449efedb96218
ABLeducation <155818284+ABLeducation@users.noreply.github.com>
2024-01-27 15:14:29 +0530

    Update toolbox.ts

--- src/blocks/math/toolbox.ts
+++ src/blocks/math/toolbox.ts
@@ -1,7 +1,7 @@
 import { COLOR_THEME } from "../../core/blockly/constants/colors";
 
 export default `
-<category name="Math" colour="${COLOR_THEME.VALUES}">
+<category name="Math" colour="${COLOR_THEME.DATA}">
      <block type="math_number">
        <field name="NUM">123</field>
      </block>
