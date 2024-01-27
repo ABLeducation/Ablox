fe1e4272ff3fb13f613b251de9924b28e06ef6cf
ABLeducation <155818284+ABLeducation@users.noreply.github.com>
2024-01-27 12:41:40 +0530

    Update theme.ts

--- src/core/blockly/theme.ts
+++ src/core/blockly/theme.ts
@@ -4,49 +4,49 @@ import type { BlocklyThemeOptions } from 'blockly';
 export const theme: BlocklyThemeOptions = {
   blockStyles: {
     logic_blocks: {
-      colourPrimary: COLOR_THEME.CONTRO,
+      colourPrimary: COLOR_THEME.CONTROL,
       colourSecondary: COLOR_THEME.CONTROL,
       colourTertiary: COLOR_THEME.CONTROL,
       hat: '',
     },
     loop_blocks: {
-      colourPrimary: COLOR_THEME.CONTRO,
+      colourPrimary: COLOR_THEME.CONTROL,
       colourSecondary: COLOR_THEME.CONTROL,
       colourTertiary: COLOR_THEME.CONTROL,
       hat: '',
     },
     procedure_blocks: {
-      colourPrimary: COLOR_THEME.CONTRO,
+      colourPrimary: COLOR_THEME.CONTROL,
       colourSecondary: COLOR_THEME.CONTROL,
       colourTertiary: COLOR_THEME.CONTROL,
       hat: '',
     },
     math_blocks: {
-      colourPrimary: COLOR_THEME.CONTRO,
+      colourPrimary: COLOR_THEME.CONTROL,
       colourSecondary: COLOR_THEME.VALUES,
       colourTertiary: COLOR_THEME.VALUES,
       hat: '',
     },
     text_blocks: {
-      colourPrimary: COLOR_THEME.CONTRO,
+      colourPrimary: COLOR_THEME.CONTROL,
       colourSecondary: COLOR_THEME.VALUES,
       colourTertiary: COLOR_THEME.VALUES,
       hat: '',
     },
     colour_blocks: {
-      colourPrimary: COLOR_THEME.CONTRO,
+      colourPrimary: COLOR_THEME.CONTROL,
       colourSecondary: COLOR_THEME.VALUES,
       colourTertiary: COLOR_THEME.VALUES,
       hat: '',
     },
     variable_blocks: {
-      colourPrimary: COLOR_THEME.CONTRO,
+      colourPrimary: COLOR_THEME.CONTROL,
       colourSecondary: COLOR_THEME.DATA,
       colourTertiary: COLOR_THEME.DATA,
       hat: '',
     },
     list_blocks: {
-      colourPrimary: COLOR_THEME.CONTRO,
+      colourPrimary: COLOR_THEME.CONTROL,
       colourSecondary: COLOR_THEME.DATA,
       colourTertiary: COLOR_THEME.DATA,
       hat: '',
